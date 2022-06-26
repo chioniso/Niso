7d0171cd92a913f0aa0cb28b2c623f80c9b9a589
Niso <106051811+iloveyouniso@users.noreply.github.com>
2022-06-26 17:46:44 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+let name = 'Mosh';
+console.log(name);
+// names should be menaingful and specific
+// variables cannot start with a number
+// cannot be a reserved keywoar like let or if
+// cannot contain a space or hyphen
+// varables are case-sensitive
+// format: quickBrownFoxJumped or QuickBrownFoxJumped
