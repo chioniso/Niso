132b454fdbefc5ddc2ce20966b9ed9bfa6c8a4e4
Niso <niso.mags@gmail.com>
2023-09-21 09:32:43 -0500

    Revamp & Reformatting

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Hello World');
 let name = 'Mosh';
 console.log(name);
-// names should be menaingful and specific
+// names should be meaniing and specific
 // variables cannot start with a number
 // cannot be a reserved keywoar like let or if
 // cannot contain a space or hyphen
